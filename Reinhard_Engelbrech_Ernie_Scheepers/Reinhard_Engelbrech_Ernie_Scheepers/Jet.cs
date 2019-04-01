e518deb8156e957f898fb0d405bcd9e8c4f8d271
babs scheepers <babs@mhkonstruksie.co.za>
2019-04-01 11:30:59 +0200

    Jet class updated

--- Reinhard_Engelbrech_Ernie_Scheepers/Reinhard_Engelbrech_Ernie_Scheepers/Jet.cs
+++ Reinhard_Engelbrech_Ernie_Scheepers/Reinhard_Engelbrech_Ernie_Scheepers/Jet.cs
@@ -8,10 +8,30 @@ namespace Reinhard_Engelbrech_Ernie_Scheepers
 {
     class Jet
     {
-        //Double Speed (km/h)
-        //Double/Int Fuel tank size
-        //Double Weigth total (Without inventory)
-        //Double Weigth total (With inventory)
-        //Array van class inventory (Inventory)
+        private string name;
+        private double maxSpeed;
+        private double maxAltitude;
+        private double fuelTankSize;
+        private double weightWithInventory;
+        private double weightWithoutInventory;
+        private InventoryObjects[] ArrInventoryObjects;
+
+        public double FuelTankSize { get => fuelTankSize; set => fuelTankSize = value; }
+        public double WeightWithInventory { get => weightWithInventory; set => weightWithInventory = value; }
+        public double WeightWithoutInventory { get => weightWithoutInventory; set => weightWithoutInventory = value; }
+        public double TopAltitude { get => maxAltitude; set => maxAltitude = value; }
+        public double MaxSpeed { get => maxSpeed; set => maxSpeed = value; }
+        public string Name { get => name; set => name = value; }
+        internal InventoryObjects[] ArrInventoryObjects1 { get => ArrInventoryObjects; set => ArrInventoryObjects = value; }
+
+        public Jet(string name, double maxSpeed, double maxAltitude, double fuelTankSize, double weightWithInventory, double weightWithoutInventory)
+        {
+            this.name = name;
+            this.maxSpeed = maxSpeed;
+            this.maxAltitude = maxAltitude;
+            this.fuelTankSize = fuelTankSize;
+            this.weightWithInventory = weightWithInventory;
+            this.weightWithoutInventory = weightWithoutInventory;
+        }
     }
 }
