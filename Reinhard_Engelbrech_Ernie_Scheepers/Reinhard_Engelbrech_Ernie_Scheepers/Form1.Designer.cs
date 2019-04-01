cbfb83f598fb05fc1d98cfe586bcdcdfc284e721
babs scheepers <babs@mhkonstruksie.co.za>
2019-04-01 10:38:26 +0200

    Jet class

--- Reinhard_Engelbrech_Ernie_Scheepers/Reinhard_Engelbrech_Ernie_Scheepers/Form1.Designer.cs
+++ Reinhard_Engelbrech_Ernie_Scheepers/Reinhard_Engelbrech_Ernie_Scheepers/Form1.Designer.cs
@@ -50,89 +50,98 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(1192, 532);
+            this.btnStart.Location = new System.Drawing.Point(1111, 551);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(182, 66);
+            this.btnStart.Size = new System.Drawing.Size(136, 54);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start Simulation";
             this.btnStart.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1173, 586);
+            this.pictureBox1.Size = new System.Drawing.Size(1098, 595);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // pbCannon1
             // 
             this.pbCannon1.Image = ((System.Drawing.Image)(resources.GetObject("pbCannon1.Image")));
-            this.pbCannon1.Location = new System.Drawing.Point(1192, 30);
+            this.pbCannon1.Location = new System.Drawing.Point(1111, 11);
+            this.pbCannon1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbCannon1.Name = "pbCannon1";
-            this.pbCannon1.Size = new System.Drawing.Size(60, 60);
+            this.pbCannon1.Size = new System.Drawing.Size(45, 49);
             this.pbCannon1.TabIndex = 2;
             this.pbCannon1.TabStop = false;
             // 
             // pbCannon2
             // 
             this.pbCannon2.Image = ((System.Drawing.Image)(resources.GetObject("pbCannon2.Image")));
-            this.pbCannon2.Location = new System.Drawing.Point(1258, 30);
+            this.pbCannon2.Location = new System.Drawing.Point(1202, 10);
+            this.pbCannon2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbCannon2.Name = "pbCannon2";
-            this.pbCannon2.Size = new System.Drawing.Size(60, 60);
+            this.pbCannon2.Size = new System.Drawing.Size(45, 49);
             this.pbCannon2.TabIndex = 3;
             this.pbCannon2.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(11, 63);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.Size = new System.Drawing.Size(45, 49);
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox5
             // 
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(69, 12);
+            this.pictureBox5.Location = new System.Drawing.Point(60, 10);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox5.Size = new System.Drawing.Size(45, 49);
             this.pictureBox5.TabIndex = 7;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(126, 12);
+            this.pictureBox6.Location = new System.Drawing.Point(60, 116);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox6.Size = new System.Drawing.Size(45, 49);
             this.pictureBox6.TabIndex = 8;
             this.pictureBox6.TabStop = false;
             // 
             // pictureBox7
             // 
             this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
-            this.pictureBox7.Location = new System.Drawing.Point(185, 12);
+            this.pictureBox7.Location = new System.Drawing.Point(109, 63);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox7.Size = new System.Drawing.Size(45, 49);
             this.pictureBox7.TabIndex = 9;
             this.pictureBox7.TabStop = false;
             // 
             // pictureBox8
             // 
             this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
-            this.pictureBox8.Location = new System.Drawing.Point(246, 12);
+            this.pictureBox8.Location = new System.Drawing.Point(60, 63);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox8.Size = new System.Drawing.Size(45, 49);
             this.pictureBox8.TabIndex = 10;
             this.pictureBox8.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1386, 610);
+            this.ClientSize = new System.Drawing.Size(1258, 616);
             this.Controls.Add(this.pictureBox8);
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.pictureBox6);
@@ -142,6 +151,7 @@
             this.Controls.Add(this.pbCannon1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
---
+++ Reinhard_Engelbrech_Ernie_Scheepers/Reinhard_Engelbrech_Ernie_Scheepers/Jet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reinhard_Engelbrech_Ernie_Scheepers
+{
+    class Jet
+    {
+        //Double Speed (km/h)
+        //Double/Int Fuel tank size
+        //Double Weigth total (Without inventory)
+        //Double Weigth total (With inventory)
+        //Array van class inventory (Inventory)
+    }
+}
